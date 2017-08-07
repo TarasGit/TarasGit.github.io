e6a056ba2d3d90c2935af6fb6e3ed56c3ada662e
Taras Iks <ikstaras@gmail.com>
2017-08-07 21:11:55 +0200

    added links to breadcrumbs

---
+++ assets/js/myscript.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".myhover").hover(function(){
+        $(this).css("opacity", "0.9");
+        },function(){
+        	$(this).css("opacity", "1");
+        });
+});
